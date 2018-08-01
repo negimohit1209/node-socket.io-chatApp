3e86eaea2ce6afd156560d6c82b997079e8c1802
negimohit1209 <mohitsngh1997@gmail.com>
2018-08-02 02:21:36 +0530

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
